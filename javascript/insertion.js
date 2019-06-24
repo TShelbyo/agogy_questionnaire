1c21e73f8addd4086826598d3966f9b0950d0cda
Théo Quessandier <theo.quessandier@wanadoo.fr>
2019-06-25 01:59:10 +0200

    fin insertion

--- javascript/insertion.js
+++ javascript/insertion.js
@@ -1,3 +1,19 @@
+var valeur;
+var x=$('.question_liste2 li').length;
+var tab=new Array(x);
+var tab_resultat=new Array(x);
+var m=$('.l_reponse #reponse').length;
+
+//initialisation tableau
+
+for(var i=1;i<=x;i++) {
+	tab[i]=0;
+}
+
+for(var i=0;i<x;i++) {
+	tab_resultat[i]=0;
+}
+
 function new_module() {
 
 	$("#new_module").css('display','block');
@@ -8,16 +24,149 @@ function f_new_question() {
 }
 
 function f_ajouter_reponse() {
+
 	var newDiv=document.createElement('div');
 	newDiv.id="reponse";
 	var newCheckbox=document.createElement('input');
 	newCheckbox.type='checkbox';
+	newCheckbox.id='c'+m;
 
 	var newInputText=document.createElement('input');
 	newInputText.type='text';
+	newInputText.id='r'+m;
 	newInputText.placeholder='Remplir reponse';
+	m++;
 
 	newDiv.appendChild(newCheckbox);
 	newDiv.appendChild(newInputText);
 	document.getElementById('reponse_liste').appendChild(newDiv);
 }
+
+
+$("#question_liste li").click(function (){
+	
+valeur = new String($(this).val());
+var index = $("ul li").index(this);
+index=index-2; //a modifier car commence à 3
+
+tab[index]++;
+tab[index]=tab[index]%2;
+if(tab[index]==1) {
+	tab_resultat[index]=valeur;
+}
+else {
+	tab_resultat[index]=0;
+}
+
+console.log(tab[index]);
+});
+
+function ajouter_new_module() {
+	var nom_module=document.getElementById('i_nom_module').value;
+
+		$.ajax({ 
+		   type: "POST", 
+		   url: "../php/insertion.php", 
+		   data: {nom_nouveau_module : nom_module}, 
+		  success: function(data) { 
+				alert(data);
+			} 
+		});
+}
+
+function ajouter_new_question() {
+
+	var nom_question=document.getElementById('i_nouvelle_question').value;
+	var type_question=document.getElementById('i_type_question').value;
+	var module_liste1=document.getElementById('id_module').value;
+
+		$.ajax({ 
+		   type: "POST", 
+		   url: "../php/insertion.php", 
+		   data: {nom_nouvelle_question : nom_question, fk_type_question : type_question, num_module1 : module_liste1}, 
+		  success: function(data) { 
+				alert(data);
+			} 
+		});
+
+		ajouter_new_reponse();
+
+		//window.location.reload(true);
+}
+
+function ajouter_new_reponse() {
+	var i=0;
+	var nb_reponses=$('.l_reponse #reponse').length;
+	var tab = new Array();
+	var tab_checkbox = new Array();
+
+	for(var j=0;j<nb_reponses;j++) {
+		var tmp=document.getElementById('r'+j).value;
+	
+		if(tmp!="") {
+			tab[i]=tmp;
+			
+
+			if(document.getElementById('c'+j).checked) {
+				tab_checkbox[i]=1;
+			}
+			else {
+				tab_checkbox[i]=0;
+			}
+
+			i++;
+		}
+	}
+
+for(var g=0;g<i;g++) {
+	$.ajax({ 
+		   type: "POST", 
+		   url: "../php/insertion.php", 
+		   data: {tab_reponse : tab[g], tab_checkbox : tab_checkbox[g]}, 
+		  success: function(data) { 
+				alert(data);
+			} 
+		});
+}
+
+}
+
+function test() {
+	var tab_questions=new Array(x);
+	var j=0;
+	var nom_questionnaire=document.getElementById('i_nom_questionnaire').value;
+
+for(var i=1;i<=tab_resultat.length;i++) {
+	if(tab_resultat[i]!=0) {
+		console.log(j);
+		tab_questions[j]=tab_resultat[i];
+		j++;
+	}
+
+}
+		$.ajax({ 
+		   type: "POST", 
+		   url: "../php/insertion.php", 
+		   data: {nom_questionnaire : nom_questionnaire, tab1 : tab_questions}, 
+		  success: function(data) { 
+				alert(data);
+			} 
+		});
+}
+
+
+
+$("#module select").change(function(){
+
+ var nom_module_liste=document.getElementById('id_module').value;
+ 		alert(nom_module_liste);
+		$.ajax({ 
+		   type: "POST", 
+		   url: "../php/insertion.php", 
+		   data: {module_liste1 : nom_module_liste}, 
+		  success: function(data) { 
+				alert(data);
+			} 
+		});
+	//window.location.reload(true);
+ });
--- php/bdd.php
+++ php/bdd.php
@@ -27,7 +27,7 @@ function connecionDb($chemin,$nomBdd,$user,$mdp) {
 
 
 function display_module($bdd) {
-	$reponse = $bdd->query('SELECT id_module, nom_module FROM module ORDER BY nom_module');
+	$reponse = $bdd->query('SELECT id_module, nom_module FROM module');
 
 	while ($donnees = $reponse->fetch()) {
 		echo '<option value="'.$donnees['id_module'].'">'.$donnees['nom_module'];
@@ -39,7 +39,17 @@ function display_module($bdd) {
 }
 
 function display_questions($bdd) {
-	$reponse = $bdd->query('SELECT id_question, nom_question FROM question');
+
+	if(!empty($_POST['module_liste1'])) {
+			echo "tyuiokjh";
+		$nom_module=$_POST['module_liste1'];
+		echo $nom_module;
+		$reponse = $bdd->query('SELECT id_question, nom_question FROM question JOIN module_question ON id_question=fk_question where fk_module='+$nom_module+')');
+	}
+	else {
+		$reponse = $bdd->query('SELECT id_question, nom_question FROM question JOIN module_question ON id_question=fk_question where fk_module=(select id_module from module where nom_module="Incendie")');
+	}
+//}
 
 	while ($donnees = $reponse->fetch()) {
 		echo '<li value="'.$donnees['id_question'].'">'.$donnees['nom_question'];
@@ -50,8 +60,9 @@ function display_questions($bdd) {
 
 }
 
-function display_type_question($bdd) {
-	$reponse = $bdd->query('SELECT id_type_question, nom_type_question FROM type_question');
+function display_type_question($bdd,$num_module) {
+	
+		$reponse = $bdd->query('SELECT id_type_question, nom_type_question FROM type_question');
 
 	while ($donnees = $reponse->fetch()) {
 		echo '<option value="'.$donnees['id_type_question'].'">'.$donnees['nom_type_question'];
@@ -65,13 +76,6 @@ function display_type_question($bdd) {
 function prepared_queries_questionnaire_insertion($bdd) {
 
 	$req=$bdd->prepare('INSERT INTO questionnaire(nom_questionnaire) VALUES(:nom_questionnaire)');
-=======
-function prepared_queries_artist_insertion($bdd) {
-
-	$req=$bdd->prepare('INSERT INTO artiste(nom_artiste,compte_instagram,compte_spotify,compte_youtube,nationalite) VALUES(:nom,:instagram,:spotify,:youtube,:nationalite)');
-
-	return $req;
-}
 
 	return $req;
 }
@@ -101,6 +105,58 @@ function execute_queries_module_insertion($bdd,$nom_module) {
 		));
 }
 
+function prepared_queries_question_insertion($bdd) {
+
+	$req=$bdd->prepare('INSERT INTO question(nom_question,fk_type_question) VALUES(:nom_question,:fk_type_question)');
+
+	return $req;
+}
+
+function execute_queries_question_insertion($bdd,$nom_question,$fk_type_question) {
+
+	$req=prepared_queries_question_insertion($bdd);
+
+		$req->execute(array(	
+		':nom_question' => $nom_question,
+		':fk_type_question' => $fk_type_question
+		));
+}
+
+function prepared_queries_liaison_question_reponse_insertion($bdd) {
+	echo 'bonjourkjhg';
+	$req=$bdd->prepare('INSERT INTO question_reponse(fk_question,fk_reponse,juste) VALUES((select max(id_question) from question),(select max(id_reponse) from reponse),:juste)');
+
+	return $req;
+}
+
+function execute_queries_liaison_question_reponse_insertion($bdd,$juste) {
+	echo 'meeeeeeeeeerdee';
+	$req=prepared_queries_liaison_question_reponse_insertion($bdd);
+
+			$req->execute(array(	
+			':juste' => $juste
+			));
+}
+
+function prepared_queries_reponse_insertion($bdd) {
+
+	$req=$bdd->prepare('INSERT INTO reponse(nom_reponse) VALUES(:nom_reponse)');
+
+	return $req;
+}
+
+function execute_queries_reponse_insertion($bdd,$nom_reponse) {
+
+	$req=prepared_queries_reponse_insertion($bdd);
+
+		//for($i=0;$i<count($nom_reponse);$i++) {
+			$req->execute(array(	
+			':nom_reponse' => $nom_reponse
+			));
+		//}
+
+}
+
 function prepared_queries_liaison_questionnaire_module_insertion($bdd) {
 
 	$req=$bdd->prepare('INSERT INTO questionnaire_module(fk_questionnaire,fk_module) VALUES((select max(id_questionnaire) from questionnaire),:num_module)');
@@ -118,7 +174,37 @@ function execute_queries_liaison_questionnaire_module_insertion($bdd,$num_module
 		));
 }
 
+function prepared_queries_liaison_module_question_insertion($bdd) {
 
+	$req=$bdd->prepare('INSERT INTO module_question(fk_module,fk_question) VALUES(:num_module,(select max(id_question) from question))');
+
+	return $req;
+}
+
+function execute_queries_liaison_module_question_insertion($bdd,$num_module) {
+	$req=prepared_queries_liaison_module_question_insertion($bdd);
+
+			$req->execute(array(	
+			':num_module' => $num_module
+			));
+}
+
+function prepared_queries_liaison_questionnaire_question_insertion($bdd) {
+
+	$req=$bdd->prepare('INSERT INTO questionnaire_question(fk_questionnaire,fk_question) VALUES((select max(id_questionnaire) from questionnaire),:fk_question)');
+
+	return $req;
+}
+
+function execute_queries_liaison_questionnaire_question_insertion($bdd,$fk_question) {
+	$req=prepared_queries_liaison_questionnaire_question_insertion($bdd);
+
+		for($i=0;$i<count($fk_question);$i++) {
+			$req->execute(array(
+			':fk_question' => $fk_question[$i]
+		));
+	}
+}
 ?>
 
    	  			
